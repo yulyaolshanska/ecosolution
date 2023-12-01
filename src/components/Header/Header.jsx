f94128c3c58760febf04a203e1d72bbf3ec69fb4
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-12-01 23:26:52 +0200

    feat: mobile header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Logo from '../Logo/Logo';
+import styles from './Header.module.scss';
 
 const Header = () => {
+    const [transparent, setTransparent] = useState(true);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const isTop = window.scrollY < (isMobile ? 120 : 70);
+            setTransparent(isTop);
+        };
+
+        setIsMobile(window.innerWidth <= 768);
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [isMobile]);
     return (
         <header>
-            <div>Header</div>
+            <div className={styles.headerContainer}>
+                <Logo />
+                <button className={styles.burgerBtn}></button>
+            </div>
         </header>
     );
 };
