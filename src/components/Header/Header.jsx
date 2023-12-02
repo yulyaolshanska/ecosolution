d30848003e7e082c50e54010acb00ee43201ce3b
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-12-03 00:49:47 +0200

    feat: toggle BurgerMenu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import { ReactComponent as BurgerBtn } from '../../assets/icons/burger.svg';
+import BurgerMenu from '../BurgerMenu/BurgerMenu';
 import Logo from '../Logo/Logo';
 import styles from './Header.module.scss';
 
 const Header = () => {
     const [transparent, setTransparent] = useState(true);
     const [isMobile, setIsMobile] = useState(false);
+    const [isMenuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -19,12 +22,20 @@ const Header = () => {
             window.removeEventListener('scroll', handleScroll);
         };
     }, [isMobile]);
+
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <header>
+        <header className={transparent ? styles.transparentHeader : ''}>
             <div className={styles.headerContainer}>
                 <Logo />
-                <button className={styles.burgerBtn}></button>
+                <button className={styles.burgerBtn} onClick={toggleMenu}>
+                    <BurgerBtn />
+                </button>
             </div>
+            <BurgerMenu handleClick={toggleMenu} isMenuOpen={isMenuOpen} />
         </header>
     );
 };
