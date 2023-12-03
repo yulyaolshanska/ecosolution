a3221956c777ae7f29e33424c90649d99e4bc077
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-12-03 14:05:30 +0200

    fest: section About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { aboutData } from '../../data/about';
+import styles from './About.module.scss';
+import AboutCard from './AboutCard/AboutCard';
+import AboutImage1 from '../../assets/images/about/wind-farms-fields.jpg';
+import AboutImage2 from '../../assets/images/about/man-worker-firld-by-solar-panels.jpg';
 
 const About = () => {
-    return <>About</>;
+    return (
+        <ul className={styles.aboutList}>
+            {aboutData.slice(0, 2).map(({ id, text, icon, title }) => (
+                <AboutCard text={text} icon={icon} title={title} key={id} />
+            ))}
+            <li className={styles.cardImage}>
+                <img className={styles.aboutImage} src={AboutImage1} alt="" />
+            </li>
+            <li className={styles.cardImage}>
+                <img className={styles.aboutImage} src={AboutImage2} alt="" />
+            </li>
+            {aboutData.slice(2, 4).map(({ id, text, icon, title }) => (
+                <AboutCard text={text} icon={icon} title={title} key={id} />
+            ))}
+        </ul>
+    );
 };
 
 export default About;
---
+++ src/data/about.js
@@ -0,0 +1,31 @@
+import OpennessIcon from '../assets/icons/about/maximize-circle.svg';
+import ResponsibilityIcon from '../assets/icons/about/global-edit.svg';
+import InnovationsIcon from '../assets/icons/about/cpu-charge.svg';
+import QualityIcon from '../assets/icons/about/ranking.svg';
+
+export const aboutData = [
+  {
+    id: 1,
+    icon: OpennessIcon,
+    title: 'Openness',
+    text: 'to the world, people, new ideas and projects',
+  },
+  { 
+    id: 2,
+    icon: ResponsibilityIcon,
+    title: 'Responsibility',
+    text: 'we are aware that the results of our work have an impact on our lives and the lives of future generations',
+  },
+  {
+    id: 3,
+    icon: InnovationsIcon,
+    title: 'Innovation',
+    text: 'we use the latest technology to implement non-standard solutions',
+  },
+  {
+    id: 4,
+    icon: QualityIcon,
+    title: 'Quality',
+    text: 'we do not strive to be the first among others, but we want to be the best in our business',
+  },
+];
