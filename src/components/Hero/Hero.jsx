5b680bb67c28f277f8e04d8709de02663e4966a7
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-12-04 01:28:22 +0200

    refactore: hero image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,42 +6,46 @@ import heroImageDesktop from '../../assets/images/hero/hero-1280w.jpg';
 import styles from './Hero.module.scss';
 
 const Hero = () => {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>RENEWABLE ENERGY For any task</h1>
-      <p className={styles.desc}>
-        Development and implementation of renewable non-polluting energy
-        sources, generating power generation using energy wind, sun, water,
-        biomass
-      </p>
-      <ButtonWithText text="Learn more" variant="learnMoreBtn" hash="/#cases">
-        <ArrowRight />
-      </ButtonWithText>
-      <div className={styles.line}></div>
-      <div className={styles.address}>
-        <p className={styles.addressText}>
-           79005, Ukraine, Lvivstreet. Shota Rustaveli, 7
-        </p>
-        <a href="mailto:office@ecosolution.com" className={styles.mail}>
-          office@ecosolution.com
-        </a>
-        <p className={styles.copyright}>ecosolution © 2023</p>
-      </div>
-      <img
-        className={styles.heroImage}
-        alt="Wind turbine"
-        srcSet={`
-    ${heroImage} 767w,
-    ${heroImageTablet} 1280w,
-    ${heroImageDesktop} 1920w
+    return (
+        <div className={styles.container} id="main">
+            <h1 className={styles.title}>RENEWABLE ENERGY For any task</h1>
+            <p className={styles.desc}>
+                Development and implementation of renewable non-polluting energy
+                sources, generating power generation using energy wind, sun,
+                water, biomass
+            </p>
+            <ButtonWithText
+                text="Learn more"
+                variant="learnMoreBtn"
+                hash="/#cases"
+            >
+                <ArrowRight />
+            </ButtonWithText>
+            <div className={styles.line}></div>
+            <div className={styles.address}>
+                <p className={styles.addressText}>
+                    79005, Ukraine, Lvivstreet. Shota Rustaveli, 7
+                </p>
+                <a href="mailto:office@ecosolution.com" className={styles.mail}>
+                    office@ecosolution.com
+                </a>
+                <p className={styles.copyright}>ecosolution © 2023</p>
+            </div>
+            <img
+                className={styles.heroImage}
+                alt="Wind turbine"
+                srcSet={`
+    ${heroImage} 480w,
+    ${heroImageTablet} 767w,
+    ${heroImageDesktop} 1280w
   `}
-        sizes="(max-width: 767px) 100vw,
-         (min-width: 768px) and (max-width: 1279px) 100vw,
+                sizes="(max-width: 767px) 480px,
+         (min-width: 768px) and (max-width: 1279px) 767px,
          (min-width: 1280px) 1280px"
-        src={heroImage}
-      />
-    </div>
-  );
+                src={heroImage}
+            />
+        </div>
+    );
 };
 
 export default Hero;
