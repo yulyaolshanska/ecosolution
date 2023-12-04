6a9079a690e7096b73eaadcc2798803aec6484c0
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-12-04 17:41:13 +0200

    feat: added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as ArrowRight } from '../../assets/icons/arrow-right.svg';
+import { ReactComponent as Facebook } from '../../assets/icons/contacts/facebook.svg';
+import { ReactComponent as Instagram } from '../../assets/icons/contacts/instagram.svg';
+import Logo from '../Logo/Logo';
+import styles from './Footer.module.scss';
 
 const Footer = () => {
-    return <>Footer</>;
+    return (
+        <footer className={styles.container} id="footer">
+            <div className={styles.navConainer}>
+                <div className={styles.logo}>
+                    <Logo />
+                </div>
+                <Link to="#main" className={styles.linkUp}>
+                    <ArrowRight />
+                </Link>
+                <div className={styles.socials}>
+                    <div className={styles.socialItem}>
+                        <Facebook />
+                    </div>
+                    <div className={styles.socialItem}>
+                        <Instagram />
+                    </div>
+                </div>
+            </div>
+            <div className={styles.contactInfo}>
+                <address>
+                    <p> 79005, Ukraine, Lvivstreet. Shota Rustaveli, 7</p>
+                </address>
+                <Link href="mailto:office@ecosolution.com">
+                    <p>office@ecosolution.com</p>
+                </Link>
+                <p>ecosolution © 2023</p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
