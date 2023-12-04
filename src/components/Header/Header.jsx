89bf7b53b37f5aef4f1f33f9c6baed1ff44beeac
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-12-04 19:53:24 +0200

    feat: styles tablet for header
    added Get in touch btn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { ReactComponent as BurgerBtn } from '../../assets/icons/burger.svg';
+import { ReactComponent as ArrowBottom } from '../../assets/icons/arrow-bottom.svg';
 import BurgerMenu from '../BurgerMenu/BurgerMenu';
+import ButtonWithText from '../Buttons/ButtonWithText';
+import useHandleNavLinkClick from '../../utils/handleNavLinkClick';
 import Logo from '../Logo/Logo';
 import styles from './Header.module.scss';
 
 const Header = () => {
     const [isMenuOpen, setMenuOpen] = useState(false);
     const [isScrolled, setIsScrolled] = useState(false);
+    const handleNavLinkClick = useHandleNavLinkClick();
 
     useEffect(() => {
         const handleScroll = () => {
@@ -21,13 +25,6 @@ const Header = () => {
         };
     }, []);
 
-    //  const handleGetInTouchClick = () => {
-    //      const contactUsSection = document.getElementById('contactUsSection');
-    //      if (contactUsSection) {
-    //          contactUsSection.scrollIntoView({ behavior: 'smooth' });
-    //      }
-    //  };
-
     const toggleMenu = () => {
         setMenuOpen(!isMenuOpen);
     };
@@ -40,9 +37,19 @@ const Header = () => {
         >
             <div className={styles.headerContainer}>
                 <Logo />
-                <button className={styles.burgerBtn} onClick={toggleMenu}>
-                    <BurgerBtn />
-                </button>
+                <div className={styles.buttonsContainer}>
+                    <button className={styles.burgerBtn} onClick={toggleMenu}>
+                        <BurgerBtn />
+                    </button>
+                    <ButtonWithText
+                        text="Get in touch"
+                        onClick={toggleMenu}
+                        variant="getInTouch"
+                        handleClick={e => handleNavLinkClick(e, '#contacts')}
+                    >
+                        <ArrowBottom />
+                    </ButtonWithText>
+                </div>
             </div>
             <BurgerMenu handleClick={toggleMenu} isMenuOpen={isMenuOpen} />
         </header>
