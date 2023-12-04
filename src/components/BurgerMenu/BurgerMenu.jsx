f0e4683ff7c0ff159d6181918381b418d4636ff0
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-12-04 17:40:58 +0200

    feat: handleMenuLinkClick

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Link, useNavigate } from 'react-router-dom';
 import { ReactComponent as Close } from '../../assets/icons/close.svg';
 import { ReactComponent as Facebook } from '../../assets/icons/facebook.svg';
 import { ReactComponent as Instagram } from '../../assets/icons/instagram.svg';
@@ -16,10 +16,34 @@ const navigation = [
 ];
 
 const BurgerMenu = ({ handleClick, isMenuOpen }) => {
+    const navigate = useNavigate();
+
+    const handleMenuLinkClick = (e, to) => {
+        e.preventDefault();
+        const targetElement = document.querySelector(to);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+            });
+
+            navigate(to);
+        }
+
+        handleClick(e);
+    };
+
+    const handleBackdropClick = e => {
+        if (e.target === e.currentTarget) {
+            handleClick(e);
+        }
+    };
+
     return (
         <>
             {isMenuOpen && (
                 <div
+                    onClick={handleBackdropClick}
                     className={` ${styles.backdrop} ${
                         isMenuOpen ? styles.open : ''
                     }`}
@@ -29,14 +53,17 @@ const BurgerMenu = ({ handleClick, isMenuOpen }) => {
                             className={styles.menuBtnClose}
                             onClick={e => handleClick(e)}
                         >
-                            <Close />{' '}
+                            <Close />
                             <span className={styles.closeIconText}>close</span>
                         </button>
                         <div className={styles.menuLine}></div>
                         <div className={styles.menuNavContainer}>
                             <nav className={styles.menuNav}>
                                 {navigation.map(({ id, name, to }) => (
-                                    <NavLink
+                                    <Link
+                                        onClick={e =>
+                                            handleMenuLinkClick(e, to)
+                                        }
                                         to={to}
                                         className={`${styles.menuLink} ${
                                             location.hash === to
@@ -49,7 +76,7 @@ const BurgerMenu = ({ handleClick, isMenuOpen }) => {
                                             {name}
                                         </span>
                                         <ArrowUpRight />
-                                    </NavLink>
+                                    </Link>
                                 ))}
                             </nav>
                             <div className={styles.menuSocials}>
